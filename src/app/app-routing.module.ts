c13963e70c16bb93aa4d4b55d281bcd6a2dbd0e7
Libor Horak <horaklibor@mail.muni.cz>
2024-01-22 15:56:16 +0100

    Random page added using ionic generate page random. New tab for Random page added (tabs.page.html) Tabs routing altered accordingly (tabs-routing.module.ts)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'random',
+    loadChildren: () => import('./random/random.module').then( m => m.RandomPageModule)
   }
 ];
 @NgModule({
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        path: 'random',
+        loadChildren: () => import('../random/random.module').then(m => m.RandomPageModule)
       },
       {
         path: 'tab2',
@@ -21,14 +21,14 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/random',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/random',
     pathMatch: 'full'
   }
 ];
